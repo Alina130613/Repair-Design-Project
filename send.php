25616d203282d5db4f643392babf32246b200fb3
Alina130613 <92860068+Alina130613@users.noreply.github.com>
2022-02-16 00:40:34 +0300

    Add files via upload

---
+++ send.php
@@ -0,0 +1,39 @@
+<?php
+
+$userName = &_POST['userName'];
+$userEmail = &_POST['userEmail'];
+$userPhone = &_POST['userPhone'];
+
+//Load Composer's autoloader
+require 'vendor/Exception.php';
+require 'vendor/PHPMailer.php';
+require 'vendor/SMTP.php';
+
+//Create an instance; passing `true` enables exceptions
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                                        //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                      //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                 //Enable SMTP authentication
+    $mail->Username   = 'astanko832';                        //SMTP username
+    $mail->Password   = 'F2n-iBN-dar-Uaw';                  //SMTP password
+    $mail->SMTPSecure = 'ssl';                             //Enable implicit TLS encryption
+    $mail->Port       = 465;                              //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('astanko832', 'Alina');
+    $mail->addAddress('astanko833');     //Add a recipient
+
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Новая заявка сайта';
+    $mail->Body    = "Имя пользователя: ${userName}, его телефон: ${userPhone}. Его почта: ${userEmail}";
+
+    $mail->send();
+    header('Location: thanks.html')
+}   catch (Exception $e) {
+    echo "Сообщение не отправленно, есть ошибка. Код ошибки: {$mail->ErrorInfo}";
+}
